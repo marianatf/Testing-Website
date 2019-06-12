62ec146a25e2d6fe4144770ec5e515bed95bf052
Your Name <you@example.com>
2019-06-12 11:13:59 -0400

    'act'

--- site/js/main.js
+++ site/js/main.js
@@ -1,27 +1,14 @@
-$(document).ready(function() {
-	$('.gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-			titleSrc: function(item) {
-				return  '<medium>&copy Carmen Zamora</medium>';
-			}
-		}
-	});
-});
-
-// Random 
-var imagesArray = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"];
-function displayImage(){
-    var num = Math.floor(Math.random() * 7); // 0...6
+function openNav(){
+	document.getElementById('nav').style.width="100%";
 }
-var img_rnd = new Array ("1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg");
-var i = Math.floor(4*Math.random());
+
+function closeNav(){
+	document.getElementById('nav').style.width="0%";
+}
+function openNav(){
+	document.getElementById('nav').style.height="100%";
+}
+
+function closeNav(){
+	document.getElementById('nav').style.height="0%";
+}
