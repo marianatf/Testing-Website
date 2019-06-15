fb5839a2bfdc155ed9cfcad231b1384467055785
Your Name <you@example.com>
2019-06-15 10:04:06 -0400

    act

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,4 @@
+// Overlay
 function openNav(){
 	document.getElementById('nav').style.width="100%";
 }
@@ -12,3 +13,43 @@ function openNav(){
 function closeNav(){
 	document.getElementById('nav').style.height="0%";
 }
+
+// Magnific Popup
+$(".gallery").magnificPopup({
+	delegate: 'a',
+	type: 'image',
+	gallery:{
+		enabled:true
+	}
+})
+
+// Sticky navbar
+	$(window).scroll(function(){
+
+		var top = $(window).scrollTop();
+		if(top>=60){
+			$("nav").addClass('secondary');
+		}
+		else 
+			if($("nav").hasClass('secondary')){
+
+				$("nav").removeClass('secondary');
+			}
+
+	});
+
+// Return to top
+
+	$(window).scroll(function(){
+		if($(this).scrollTop()>30){
+			$('#back').fadeIn();
+		} else{
+			$('#back').fadeOut();
+		}
+	});
+
+	$("#back").click(function(){
+		$('html ,body').animate({scrollTop:0},0);
+
+	});
+
